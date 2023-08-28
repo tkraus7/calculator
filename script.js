62b074c782aba379e7ade8c029610d80ca4020c7
Kraus, Tomáš <krausto8@fel.cvut.cz>
2023-08-28 18:18:13 +0200

    implemented dot, sign and backspace

--- script.js
+++ script.js
@@ -10,11 +10,23 @@ function changeRes(elem) {
     }
 }
 
-console.log(buttons)
-
 for (num_button of buttons) {
     num_button.addEventListener("click", changeRes)
 }
+
+document.getElementById("dot_button").addEventListener("click", e => {
+    if (result.innerText.length < 13 && !result.innerText.includes('.')) {
+        result.innerText = result.innerText + '.'
+    }
+})
+
+document.getElementById("sign_button").addEventListener("click", e => {
+    if (result.innerText.includes('-')) {
+        result.innerText = result.innerText.substring(1, result.innerText.length)
+    } else {
+        result.innerText = '-' + result.innerText
+    }
+})
 
 document.getElementById("ce_button").addEventListener("click", e => {
     result.innerText = '0'
@@ -23,4 +35,13 @@ document.getElementById("ce_button").addEventListener("click", e => {
 document.getElementById("c_button").addEventListener("click", e => {
     result.innerText = '0'
     buffer.innerText = '0'
+})
+
+document.getElementById("backspace_button").addEventListener("click", e => {
+    result.innerText = result.innerText.length == 1 ?
+     '0' : result.innerText.substring(0, result.innerText.length - 1)
+})
+
+document.getElementById("plus_button").addEventListener("click", () => {
+
 })
